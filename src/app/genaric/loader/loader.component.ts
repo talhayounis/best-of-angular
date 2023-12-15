36f78be388c8512e674bec100cd651671403f4ed
talhayounis <talhayounis.xperts@gmail.com>
2023-12-16 00:11:40 +0500

    ivariable initialization

--- src/app/genaric/loader/loader.component.ts
+++ src/app/genaric/loader/loader.component.ts
@@ -9,6 +9,7 @@ export class LoaderComponent {
 
   loaderName: string = "Hold tight, Loading";
   subTitle: string = 'Sub headings';
+  numberOfSection: number = 2;
   constructor(){}
   ngOnInit(){}
 }
