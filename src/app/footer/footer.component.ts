d0eea6a2593d5d89f0e8dab300cc10d188810417
talhayounis <talhayounis.xperts@gmail.com>
2024-01-24 00:29:01 +0500

    go footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -9,6 +9,7 @@ export class FooterComponent {
   title: string = 'footer';
   subTitle: string ='footer sub title';
   footerNumber: number = 11;
+  goFooter: string = 'Lets go footer';
 constructor(){
 
 }
